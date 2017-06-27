4992b04c753a292c2fd00a2660190be12fc5493e
Xander_ <alessandro.cevoli2@studio.unibo.it>
2017-06-27 19:00:24 +0200

    - Added ASynch Request handling - Successfully managed GET with parameters! POST shouldn't be more difficult - Refactored code to match an OO design

--- src/main/scala/http/AkkaHttpServer.scala
+++ src/main/scala/http/AkkaHttpServer.scala
@@ -1,10 +1,5 @@
 package http
 
-/**
-  * Created by Alessandro on 27/06/2017.
-  */
-
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpMethods._
@@ -12,17 +7,23 @@ import akka.http.scaladsl.model._
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 
+import scala.concurrent.Future
 import scala.io.StdIn
 
-class AkkaHttpServer {
+/**
+  * A simple Akka-Http server with Asynchronous Handler and Unblocking Dispatcher
+  *
+  * The implementation is based on the code provided by the Akka Documentation
+  * and this gitHub Repo @link{https://gist.github.com/josdirksen/5710f1e65605fdc85c52}
+  *
+  * @author Xander_C{AlessandroCevoli2@studio.unibo.it};
+  * @groupname Arianna;
+  *
+  */
+class AkkaHttpServer(name : String) {
     
-}
-
-object AkkaHttpServer {
-    
-    def main(args: Array[String]) {
-        implicit val customConfig = ConfigFactory.parseString(
-            """
+   private implicit val config = ConfigFactory.parseString(
+        """
               http-akka-blocking-dispatcher {
                 |type = Dispatcher
                 |executor = "thread-pool-executor"
@@ -31,40 +32,75 @@ object AkkaHttpServer {
                 }
                 |throughput = 100
               }
-            """.stripMargin)
-        implicit val defaultConfig = ConfigFactory.load()
-        
-        implicit val config = customConfig.withFallback(defaultConfig)
-        
-        implicit val system = ActorSystem("Arianna-Http-Master", config)
-        
-        implicit val materializer = ActorMaterializer()
-        // needed for the future map/flatmap in the end
-        //implicit val executionContext = system.dispatcher
-        implicit val blockingDispatcher = system.dispatchers.lookup("http-akka-blocking-dispatcher")
-        
-        val requestHandler: HttpRequest => HttpResponse = {
-            case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+            """.stripMargin).withFallback(ConfigFactory.load())
+    
+    implicit val akkaSubSystem = ActorSystem(name, config)
+    
+    private implicit val materializer = ActorMaterializer()
+    
+    private implicit val blockingDispatcher = akkaSubSystem.dispatchers.lookup("http-akka-blocking-dispatcher")
+    
+    def bind(hostname : String, port : Int) : Future[Http.ServerBinding] =
+        Http().bindAndHandleAsync(aSyncRequestHandler, hostname, port)
+    
+    // All the Futures are going to be handled by the Blocking Dispatcher
+    def aSyncRequestHandler(request: HttpRequest): Future[HttpResponse] = request match {
+        case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+            
+            Future {
                 HttpResponse(entity = HttpEntity(
                     ContentTypes.`text/html(UTF-8)`,
                     "<html><body>Hello world!</body></html>"))
-            
-            case HttpRequest(GET, Uri.Path("/nodes"), _, _, _) =>
+            }
+        
+        case HttpRequest(GET, Uri.Path("/nodes"), _, _, _) =>
+            Future {
                 HttpResponse(entity = HttpEntity(
                     ContentTypes.`text/html(UTF-8)`,
                     "<html><body>Here are the active nodes</body></html>"))
+            }
+        
+        case HttpRequest(GET, Uri.Path("/logs"), _, _, _) =>
             
-            case HttpRequest(GET, Uri.Path("/logs"), _, _, _) =>
+            Future {
                 HttpResponse(entity = HttpEntity(
                     ContentTypes.`text/html(UTF-8)`,
                     "<html><body>Here are the system logs!</body></html>"))
+            }
+        // To be modified with PUT or POST
+        case HttpRequest(GET, Uri.Path("/subscribe"), _, _, _) =>
             
-            case r: HttpRequest =>
-                r.discardEntityBytes() // important to drain incoming HTTP Entity stream
-                HttpResponse(404, entity = "Unknown resource!")
-        }
+            Future {
+                request.uri.query().foreach(p => println(p._1 + "=>" + p._2))
+                
+                HttpResponse(entity = HttpEntity(
+                    ContentTypes.`text/html(UTF-8)`,
+                    "<html><body>Here is the Map</body></html>"))
+            }
         
-        val bindingFuture = Http().bindAndHandleSync(requestHandler, "localhost", 8080)
+        case r: HttpRequest =>
+            r.discardEntityBytes() // important to drain incoming HTTP Entity stream
+            Future {
+                HttpResponse(404, entity = "Unknown resource!")
+            }
+    }
+    
+    def terminate() : Unit = akkaSubSystem.terminate()
+}
+
+
+object AkkaHttpServer {
+    
+    def apply(name: String) : AkkaHttpServer =
+        new AkkaHttpServer(name)
+}
+
+object AkkaServerBootstrap extends App{
+    
+    override def main(args: Array[String]) {
+        
+        val httpServer = AkkaHttpServer("Arianna.Master.http")
+        val bindingFuture = httpServer.bind("localhost", 8080)
         
         println(s"Server online at http://localhost:8080/\n")
         
@@ -74,7 +110,7 @@ object AkkaHttpServer {
         
         bindingFuture
             .flatMap(_.unbind()) // trigger unbinding from the port
-            .onComplete(_ => system.terminate()) // and shutdown when done
+            .onComplete(_ => httpServer.terminate()) // and shutdown when done
         
     }
 }
