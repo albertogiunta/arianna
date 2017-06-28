16b394289590288836b29f05b5cc6447151b51d8
Xander_ <alessandro.cevoli2@studio.unibo.it>
2017-06-28 17:09:55 +0200

    - Refactored Code -> HttpServer provide Trait and various Abstraction, AkkaHttpServer provides a concrete implementation, Bootstrap is the main - Added DSL-Route Request Handler in AkkaHttpServer

---
+++ src/main/scala/http/HttpServer.scala
@@ -0,0 +1,74 @@
+package http
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.Future
+
+/**
+  * A simple Server trait which defines both Future oriented and Route/Stream oriented approach
+  */
+trait HttpServer {
+    
+    def bindAsync(hostname: String, port: Int): Future[Http.ServerBinding]
+    
+    def bindBlocking(hostname: String, port: Int): Future[Http.ServerBinding]
+    
+    def terminate(): Unit
+}
+
+/**
+  * A simple Akka-Http server with Asynchronous Handler and Unblocking Dispatcher
+  *
+  * The implementation is based on the code provided by the Akka Documentation
+  * and this gitHub Repo @link{https://gist.github.com/josdirksen/5710f1e65605fdc85c52}
+  *
+  * @author Xander_C{AlessandroCevoli2@studio.unibo.it};
+  * @groupname Arianna;
+  *
+  */
+abstract class AbstractHttpServer(name: String) extends HttpServer {
+    
+    protected implicit val config = ConfigFactory.parseString(
+        """
+          http-akka-blocking-dispatcher {
+            |type = Dispatcher
+            |executor = "thread-pool-executor"
+            |thread-pool-executor {
+                |fixed-pool-size = 16
+            }
+            |throughput = 100
+          }
+        """.stripMargin).withFallback(ConfigFactory.load())
+    
+    implicit val akkaSubSystem = ActorSystem(name, config)
+    
+    protected implicit val materializer = ActorMaterializer()
+    
+    protected implicit val blockingDispatcher =
+        akkaSubSystem.dispatchers.lookup("http-akka-blocking-dispatcher")
+    
+    // All the Futures are going to be handled by the Blocking Dispatcher
+    val aSyncRequestHandler: HttpRequest => Future[HttpResponse] = null
+    
+    val streamRequestHandler: Route = null
+    
+    override def bindAsync(hostname: String, port: Int): Future[Http.ServerBinding] =
+        Http().bindAndHandleAsync(aSyncRequestHandler, hostname, port)
+    
+    override def bindBlocking(hostname: String, port: Int): Future[Http.ServerBinding] =
+        Http().bindAndHandle(streamRequestHandler, hostname, port)
+    
+    override def terminate(): Unit = akkaSubSystem.terminate()
+}
+
+object HttpServer {
+    
+    def apply(name: String): HttpServer =
+        new AkkaHttpServer(name)
+}
+
---
+++ src/main/scala/http/HttpServerWithStream.scala
@@ -0,0 +1,9 @@
+package http
+
+/**
+  * Created by Alessandro on 28/06/2017.
+  */
+class HttpServerWithStream(name: String) extends AkkaHttpServer(name) {
+    
+
+}
