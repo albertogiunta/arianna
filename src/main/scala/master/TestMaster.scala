04ead3f08dfb7f688fb5d6b03f082a92e4c6f3d5
Xander_ <alessandro.cevoli2@studio.unibo.it>
2017-06-28 19:51:18 +0200

    up

--- src/main/scala/master/TestMaster.scala
+++ src/main/scala/master/TestMaster.scala
@@ -6,6 +6,9 @@ import master.cluster.Subscriber
 object TestMaster extends App {
     
     val system = ActorSystem("Arianna-Master-cluster")
-    
+    //ERROR] [06/28/2017 19:49:07.571] [Arianna-Master-cluster-akka.actor.default-dispatcher-2]
+    // [akka://Arianna-Master-cluster/user/Subscriber] ActorSystem [akka://Arianna-Master-cluster]
+    // needs to have a 'ClusterActorRefProvider' enabled in the configuration,
+    // currently uses [akka.actor.LocalActorRefProvider]
     val actor = system.actorOf(Props[Subscriber], "Subscriber")
 }
