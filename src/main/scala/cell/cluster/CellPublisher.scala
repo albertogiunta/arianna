2cb1c366c3737f0a40b735ffbb54af0b1923731d
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2017-07-07 22:17:23 +0200

    Completed the setup phase for the cell subscriber

--- src/main/scala/cell/cluster/CellPublisher.scala
+++ src/main/scala/cell/cluster/CellPublisher.scala
@@ -3,17 +3,41 @@ package cell.cluster
 import akka.cluster.pubsub.DistributedPubSubMediator.Publish
 import common.BasicPublisher
 import ontologies._
+import ontologies.messages.Location._
 import ontologies.messages.MessageType._
-import ontologies.messages.{AriadneRemoteMessage, MessageType}
+import ontologies.messages._
 
 /**
   * Actor that manages the sending of messages to the main server
   * Created by Matteo Gabellini on 29/06/2017.
   */
 class CellPublisher extends BasicPublisher {
-    
+
+    /*
+    * Provisional constants that represents the cell's info.
+    * In the future this info will be modelled in a different way
+    * */
+    private val cellID: Int = 12345
+    private val cellUri: String = "uri"
+    private val cellName: String = "Gondor"
+    private val roomVertices: Coordinates = Coordinates(Point(1, 1),
+        Point(-1, -1),
+        Point(-1, 1),
+        Point(1, -1))
+    private val antennaPosition: Point = Point(0, 0)
     override protected def init(args: List[Any]) = {
         log.info("Hello there from {}!", name)
+        mediator ! Publish(Topic.HandShake,
+            AriadneRemoteMessage(
+                Handshake,
+                Handshake.Subtype.Cell2Master,
+                Location.Cell >> Location.Server,
+
+                Handshake.Subtype.Cell2Master.marshal(
+                    InfoCell(cellID, cellUri, cellName, roomVertices, antennaPosition)
+                )
+            )
+        )
     }
 
     override protected def receptive = {
