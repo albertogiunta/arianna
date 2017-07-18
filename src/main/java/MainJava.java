3cebcc59e8304baafe0a2a4fdd47adcd5f55a170
Alberto Giunta <alb.giunta@gmail.com>
2017-07-18 09:50:33 +0200

    Revert "Cell core user actor interaction"

--- src/main/java/MainJava.java
+++ src/main/java/MainJava.java
@@ -1,11 +1,6 @@
 public class MainJava {
 
-    public static void main(String[] args) throws InterruptedException {
-
-//        Vertx    vertx = Vertx.vertx();
-//        WSServer s     = new WSServer(vertx);
-//        vertx.deployVerticle(s);
-//        WSClient c = new WSClient(vertx);
-//        vertx.deployVerticle(c);
+    public static void main(String[] args) {
     }
+
 }
