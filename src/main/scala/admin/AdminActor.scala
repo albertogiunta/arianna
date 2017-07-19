1e8e8fbe3e893a7d167a9d29314111146e794f62
lisamazzini <lisa.mazzini5@gmail.com>
2017-07-19 11:52:03 +0200

    added handshake handler in adminmanager and adminactor

--- src/main/scala/admin/AdminActor.scala
+++ src/main/scala/admin/AdminActor.scala
@@ -9,6 +9,7 @@ import akka.actor.{ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 import common.CustomActor
 import ontologies.messages.Location._
+import ontologies.messages.MessageType.{Alarm, Handshake, Topology}
 import ontologies.messages._
 
 import scala.collection.mutable.ListBuffer
@@ -19,28 +20,27 @@ class AdminActor(interfaceView: InterfaceView) extends CustomActor {
     var area: Area = _
     val interfaceController: InterfaceController = interfaceView.controller
     interfaceController.actorRef = self
-    val adminManager = context.actorSelection("akka.tcp://Arianna-Cluster@192.168.0.9:25520/user/AdminManager")
+    val adminManager = context.actorSelection("akka.tcp://Arianna-Cluster@127.0.0.1:25520/user/Master/AdminManager")
     val toServer: MessageDirection = Location.Admin >> Location.Server
 
     override def receive: Receive = {
         //Ricezione dell'aggiornamento delle celle
         case msg@AriadneMessage(_, MessageType.Update.Subtype.UpdateForAdmin, _, adminUpdate: UpdateForAdmin) => {
-            println("Ricevuto update lato admin")
             val updateCells: ListBuffer[CellForView] = new ListBuffer[CellForView]
             adminUpdate.list.foreach(c => updateCells += new CellForView(c.infoCell.id, c.infoCell.name, c.currentPeople, c.sensors))
             interfaceController.updateView(updateCells.toList)
         }
         //Ricezione del messaggio iniziale dall'interfaccia con aggiornamento iniziale
-        case msg@AriadneMessage(_, MessageType.Topology.Subtype.Planimetrics, _, area: Area) => {
-            adminManager ! AriadneMessage(MessageType.Topology, MessageType.Topology.Subtype.Planimetrics, toServer, msg.content)
+        case msg@AriadneMessage(_, Topology.Subtype.Planimetrics, _, area: Area) => adminManager ! msg.copy(direction = toServer)
+
+        case msg@AriadneMessage(_, Alarm.Subtype.FromInterface, _, _) => adminManager ! msg.copy(direction = toServer)
+
+        case msg@AriadneMessage(_, Alarm.Subtype.Basic, _, _) => interfaceController.triggerAlarm()
+
+        case msg@AriadneMessage(Handshake, Handshake.Subtype.Cell2Master, _, sensorsInfo: SensorList) => {
+
         }
-        case msg@AriadneMessage(_, MessageType.Alarm.Subtype.FromInterface, _, _) => {
-            println("Allarme ricevuto dall'attore")
-            adminManager ! msg
-        }
-        case msg@AriadneMessage(_, MessageType.Alarm.Subtype.Basic, _, _) => {
-            interfaceController.triggerAlarm()
-        }
+
         case _ => println("none")
 
     }
