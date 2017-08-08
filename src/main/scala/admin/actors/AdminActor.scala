a607ee8e7414334ced1bf3701032edabfe9ff55e
lisamazzini <lisa.mazzini5@gmail.com>
2017-08-08 16:35:42 +0200

    Fixed problem with LookingForAMap message

--- src/main/scala/admin/actors/AdminActor.scala
+++ src/main/scala/admin/actors/AdminActor.scala
@@ -19,7 +19,7 @@ class AdminActor() extends BasicActor {
     //Se si fa partire solo l'admin manager
     //private val adminManager = context.actorSelection("akka.tcp://Arianna-Cluster@127.0.0.1:25520/user/AdminManager")
     //Se si fa partire il master
-    val adminManager = context.actorSelection("akka.tcp://Arianna-Cluster@127.0.0.1:25520/user/Master/AdminManager")
+    private var adminManager = context.actorSelection("akka.tcp://Arianna-Cluster@127.0.0.1:25520/user/Master/AdminManager")
     private val interfaceManager = context.actorOf(Props[InterfaceManager], NamingSystem.InterfaceManager)
     private var areaLoaded: Area = _
     private val toServer: MessageDirection = Location.Admin >> Location.Master
@@ -29,12 +29,15 @@ class AdminActor() extends BasicActor {
         interfaceManager ! AriadneMessage(Init, Init.Subtype.Greetings, toSelf, Greetings(List.empty))
     }
 
+    context.system.eventStream.subscribe(self, classOf[akka.remote.DisassociatedEvent])
+
     override def receptive: Receive = {
         //Ricezione del messaggio iniziale dall'interfaccia con aggiornamento iniziale
         case msg@AriadneMessage(_, Topology.Subtype.Planimetrics, _, area: Area) => {
             areaLoaded = area
             adminManager ! msg.copy(direction = toServer)
             context.become(operational)
+            log.info("Map loaded from GUI")
         }
 
         case _ => desist _
@@ -51,14 +54,28 @@ class AdminActor() extends BasicActor {
 
         case msg@AriadneMessage(Handshake, Handshake.Subtype.CellToMaster, _, sensorsInfo: SensorsInfoUpdate) => interfaceManager ! msg.copy(direction = toSelf)
 
-        case msg@AriadneMessage(Error, Error.Subtype.LookingForAMap, _, _) => adminManager ! AriadneMessage(Topology, Topology.Subtype.Planimetrics, toServer, areaLoaded)
-
         case msg@AriadneMessage(Error, Error.Subtype.MapIdentifierMismatch, _, _) => interfaceManager ! msg.copy(direction = toSelf)
 
         case msg@AriadneMessage(Init, Init.Subtype.Goodbyes, _, _) => adminManager ! msg.copy(direction = toServer)
 
+        case event: akka.remote.DisassociatedEvent => {
+            context.become(waitingForMaster)
+            log.info("Lost connection with Master... Waiting for LookingForAMap message")
+        }
+
         case _ => desist _
 
+    }
+
+    def waitingForMaster: Receive = {
+        case msg@AriadneMessage(Error, Error.Subtype.LookingForAMap, _, _) => {
+            adminManager = context.actorSelection("akka.tcp://Arianna-Cluster@127.0.0.1:25520/user/Master/AdminManager")
+            log.info("Connection with Master established again")
+            adminManager ! AriadneMessage(Topology, Topology.Subtype.Planimetrics, toServer, areaLoaded)
+            log.info("Map sent again to Master")
+            context.become(operational)
+        }
+        case msg => println(msg.toString)
     }
 
 }
