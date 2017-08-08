6da7230868413724a6b0e72f8e097fd6c2f14a02
albertogiunta <alb.giunta@gmail.com>
2017-08-08 10:30:11 +0200

    refactored userRun

--- src/main/scala/cell/core/UserManager.scala
+++ src/main/scala/cell/core/UserManager.scala
@@ -1,22 +1,15 @@
 package cell.core
 
-import java.io.File
-import java.nio.file.Paths
-
 import _root_.io.vertx.core.Vertx
-import akka.actor.{ActorLogging, ActorRef, ActorSystem, Props}
+import akka.actor.ActorLogging
 import cell.WSClient
 import com.actors.BasicActor
-import com.typesafe.config.ConfigFactory
 import ontologies.messages.AriannaJsonProtocol._
 import ontologies.messages.Location._
 import ontologies.messages.MessageType.Topology
 import ontologies.messages.MessageType.Topology.Subtype._
 import ontologies.messages._
 import spray.json._
-
-import scala.collection.mutable
-import scala.io.Source
 
 object MSGToAkka {
     val NORMAL_CONNECTION: String = "connect"
@@ -101,48 +94,5 @@ class UserManager extends BasicActor with ActorLogging {
 
     def getCellWithId(uri: String): RoomID = {
         areaForCell.rooms.filter(p => p.cell.uri == uri).map(f => f.info.id).head
-    }
-}
-
-object RunUser {
-
-    private def readJson(filename: String): JsValue = {
-        val source: String = Source.fromFile(filename).getLines.mkString
-        source.parseJson
-    }
-
-    def loadArea(): Area = {
-        val area = readJson(s"res/json/map15_room.json").convertTo[Area]
-        area
-    }
-
-    def areaForCell: AreaViewedFromACell = {
-        AreaViewedFromACell(area)
-    }
-
-    var area: Area = loadArea()
-
-    def main(args: Array[String]): Unit = {
-
-        val path2Project = Paths.get("").toFile.getAbsolutePath
-        val path2Config = path2Project + "/res/conf/akka/application.conf"
-        val config = ConfigFactory.parseFile(new File(path2Config))
-        val system = ActorSystem.create("userSystem", config.getConfig("user"))
-
-        val map: mutable.LinkedHashMap[String, Greetings] = mutable.LinkedHashMap()
-        val actors: mutable.MutableList[ActorRef] = mutable.MutableList()
-
-        for (i <- 1 to 15) {
-            map.put(s"user$i", Greetings(List(s"uri$i", (8080+i).toString)))
-        }
-
-        map.foreach(u => {
-            val userActor = system.actorOf(Props.create(classOf[UserManager]), u._1)
-            userActor ! AriadneMessage(MessageType.Init, MessageType.Init.Subtype.Greetings, Location.User >> Location.Self, u._2)
-            Thread.sleep(1000)
-            userActor ! AriadneMessage(Topology, ViewedFromACell, Location.User >> Location.Self, areaForCell)
-            Thread.sleep(500)
-            actors += userActor
-        })
     }
 }
