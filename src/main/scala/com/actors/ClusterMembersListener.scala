8d105bb475ba8dd499daec0aa93f319769098c21
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2017-08-10 10:31:06 +0200

    implemented watch dog for the remote message ack

--- src/main/scala/com/actors/ClusterMembersListener.scala
+++ src/main/scala/com/actors/ClusterMembersListener.scala
@@ -24,6 +24,7 @@ abstract class ClusterMembersListener extends CustomActor {
 
     override def preStart = {
         cluster.subscribe(self, classOf[MemberUp], classOf[MemberEvent])
+        log.info("I'm born")
     }
     
     override def postStop: Unit = cluster unsubscribe self
