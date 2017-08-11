5573a1b28601485a4a64fad0102b12ec29357cdb
lisamazzini <lisa.mazzini5@gmail.com>
2017-08-11 22:48:56 +0200

    Fixed tests

--- src/test/scala/admin/actors/ChartManagerTest.scala
+++ src/test/scala/admin/actors/ChartManagerTest.scala
@@ -1,5 +1,7 @@
 package scala.admin.actors
 
+import javafx.embed.swing.JFXPanel
+
 import admin.actors.ChartManager
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.testkit.{TestActorRef, TestKit, TestProbe}
@@ -30,17 +32,26 @@ class ChartManagerTest extends TestKit(ActorSystem("InterfaceManagerTest")) with
 
     private class Tester(probe: ActorRef) extends CustomActor {
 
+        val openChart = AriadneMessage(Interface, Interface.Subtype.OpenChart, Location.Admin >> Location.Self, CellForChart(RoomInfo.empty, List.empty[Int]))
+
         val interfaceManager: TestActorRef[CustomActor] =
             TestActorRef(Props(new CustomActor {
+
                 val chartManager = context.actorOf(Props[ChartManager], NamingSystem.ChartManager)
 
                 override def receive: Receive = {
+                    case "Start" => chartManager ! openChart
                     case msg if sender == chartManager => {
                         probe ! msg
                     }
                     case msg => chartManager forward msg
                 }
             }), self, NamingSystem.InterfaceManager)
+
+        override def preStart(): Unit = {
+            new JFXPanel
+            self ! "Start"
+        }
 
         override def receive: Receive = {
             //case msg if sender == adminActor => probe forward msg
