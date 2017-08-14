22a2e47c1be0b07eef1ee448c2f5e2a59b232ec0
albertogiunta <alb.giunta@gmail.com>
2017-08-14 10:18:02 +0200

    - various fixes - ported pair class to scala - changed some formatting standards

--- src/main/scala/cell/core/WSServer.java
+++ src/main/scala/cell/core/WSServer.java
@@ -1,19 +1,18 @@
 package cell.core;
 
-import com.utils.Pair;
-
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import akka.actor.ActorRef;
+import com.utils.Pair;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.ServerWebSocket;
 import ontologies.messages.RouteRequestShort;
 import ontologies.messages.RouteResponse;
 import scala.tools.jline_embedded.internal.Log;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * A Vertx implementation of several websockets needed to communicate with the end user and keep a
@@ -22,16 +21,16 @@ import scala.tools.jline_embedded.internal.Log;
  */
 public class WSServer extends AbstractVerticle {
 
-    private final Vertx    vertx;
+    private final Vertx vertx;
     private final ActorRef userActor;
 
-    private Map<String, ServerWebSocket>                                   usersWaitingForDisconnection;
-    private Map<String, ServerWebSocket>                                   usersWaitingForConnectionOk;
-    private Map<String, ServerWebSocket>                                   usersWaitingForArea;
-    private Map<String, ServerWebSocket>                                   usersReadyForAlarm;
+    private Map<String, ServerWebSocket> usersWaitingForDisconnection;
+    private Map<String, ServerWebSocket> usersWaitingForConnectionAck;
+    private Map<String, ServerWebSocket> usersWaitingForArea;
+    private Map<String, ServerWebSocket> usersReadyForAlarm;
     private Map<Pair<String, String>, List<Pair<String, ServerWebSocket>>> usersWaitingForRoute;
 
-    private String  baseUrl  = "";
+    private String baseUrl = "";
     private Integer basePort = 0;
 
     public WSServer(Vertx vertx, ActorRef userActor, String baseUrl, Integer port) {
@@ -41,7 +40,7 @@ public class WSServer extends AbstractVerticle {
         this.basePort = port;
 
         this.usersWaitingForDisconnection = new ConcurrentHashMap<>();
-        this.usersWaitingForConnectionOk = new ConcurrentHashMap<>();
+        this.usersWaitingForConnectionAck = new ConcurrentHashMap<>();
         this.usersWaitingForArea = new ConcurrentHashMap<>();
         this.usersReadyForAlarm = new ConcurrentHashMap<>();
         this.usersWaitingForRoute = new ConcurrentHashMap<>();
@@ -50,15 +49,18 @@ public class WSServer extends AbstractVerticle {
     @Override
     public void start() throws Exception {
         vertx.createHttpServer().websocketHandler(ws -> {
-            Log.info("[SERVER " + baseUrl + "] PATH " + ws.path() + " " + ws.uri() + " " + ws.query());
+            System.out.println("[SERVER " + baseUrl + "] PATH " + ws.path() + " " + ws.uri() + " " + ws.query());
             if (ws.path().equals(baseUrl + "/connect")) {
                 ws.handler(data -> {
                     Log.info("[SERVER " + baseUrl + "] GOT NEW USER | " + data.toString());
                     if (data.toString().equalsIgnoreCase(MSGTAkkaVertx.NORMAL_CONNECTION())) {
-                        this.usersWaitingForConnectionOk.put(ws.textHandlerID(), ws);
+                        System.out.println("sending normal to actor");
+                        this.usersWaitingForConnectionAck.put(ws.textHandlerID(), ws);
                     } else if (data.toString().equalsIgnoreCase(MSGTAkkaVertx.FIRST_CONNECTION())) {
+                        System.out.println("sending first to actor");
                         this.usersWaitingForArea.put(ws.textHandlerID(), ws);
                     } else if (data.toString().equalsIgnoreCase(MSGTAkkaVertx.DISCONNECT())) {
+                        System.out.println("sending disconnect to actor");
                         this.usersWaitingForDisconnection.put(ws.textHandlerID(), ws);
                     }
                     userActor.tell(data.toString(), ActorRef.noSender());
@@ -66,9 +68,9 @@ public class WSServer extends AbstractVerticle {
             } else if (ws.path().equals(baseUrl + "/route")) {
                 ws.handler(data -> {
                     Log.info("asked route " + data.toString());
-                    String               uriStart = data.toString().split("-")[0];
-                    String               uriEnd   = data.toString().split("-")[1];
-                    Pair<String, String> p        = new Pair<>(uriStart, uriEnd);
+                    String uriStart = data.toString().split("-")[0];
+                    String uriEnd = data.toString().split("-")[1];
+                    Pair<String, String> p = new Pair<>(uriStart, uriEnd);
                     usersWaitingForRoute.computeIfAbsent(p, k -> new LinkedList<>()).add(new Pair<>(ws.textHandlerID(), ws));
                     userActor.tell(new RouteRequestShort(ws.textHandlerID(), uriStart, uriEnd, false), ActorRef.noSender());
                 });
@@ -87,21 +89,27 @@ public class WSServer extends AbstractVerticle {
     }
 
     /**
-     * Called after a user asks to connect to a specific cell but was already present in the system, hence he
+     * Called after a user asks to connect to a specific cell but was already present in the system,
+     * hence he
      * doesn't need no further information about it
+     *
      * @param ack the ack message
      */
     public void sendOkToNewUser(String ack) {
-        usersWaitingForConnectionOk.values().forEach(ws -> ws.writeTextMessage(ack));
-        usersWaitingForConnectionOk.clear();
+        System.out.println("Waiting for ACK " + usersWaitingForConnectionAck.size());
+        usersWaitingForConnectionAck.values().forEach(ws -> ws.writeTextMessage(ack));
+        usersWaitingForConnectionAck.clear();
     }
 
     /**
-     * Called when the user first connects to a cell, he should receive the area, so that he can complete his work
+     * Called when the user first connects to a cell, he should receive the area, so that he can
+     * complete his work
      * on his end
+     *
      * @param area the marshaled version of the area
      */
     public void sendAreaToNewUser(String area) {
+        System.out.println("Waiting for AREA " + usersWaitingForArea.size());
         usersWaitingForArea.values().forEach(ws -> ws.writeTextMessage(area));
         usersWaitingForArea.clear();
     }
@@ -110,18 +118,21 @@ public class WSServer extends AbstractVerticle {
      * Called when a user disconnects from a cell because he wants to connect to the next one
      */
     public void disconnectUsers() {
+        System.out.println("Waiting for DISCONNECTION " + usersWaitingForDisconnection.size());
         this.usersWaitingForDisconnection.keySet().forEach(id -> {
-            usersWaitingForConnectionOk.remove(id);
+            usersWaitingForConnectionAck.remove(id);
             usersWaitingForArea.remove(id);
             usersReadyForAlarm.remove(id);
-            // remove from userswaitingforroute
+            // remove from usersWaitingForRoute
         });
         this.usersWaitingForDisconnection.clear();
     }
 
     /**
-     * Called when an alarm is the detected in the system and should be propagated to all the end users
-     * @param routeAsJson the mashaled version of the route from this cell to the exit
+     * Called when an alarm is the detected in the system and should be propagated to all the end
+     * users
+     *
+     * @param routeAsJson the marshaled version of the route from this cell to the exit
      */
     public void sendAlarmToUsers(String routeAsJson) {
         usersReadyForAlarm.forEach((s, ws) -> ws.writeTextMessage(routeAsJson));
@@ -129,12 +140,13 @@ public class WSServer extends AbstractVerticle {
 
     /**
      * Called when a route is requested from one or more users and it's finally calculated and sent
-     * @param route the RouteResponse object
+     *
+     * @param route       the RouteResponse object
      * @param routeAsJson the marshaled version of the route
      */
     public void sendRouteToUsers(RouteResponse route, String routeAsJson) {
-        Pair<String, String> p = new Pair<>("uri"+route.request().fromCell().serial(), "uri"+route.request().toCell().serial());
-        this.usersWaitingForRoute.getOrDefault(p, new LinkedList<>()).forEach(u -> u.snd.writeTextMessage(routeAsJson));
+        Pair<String, String> p = new Pair<>("uri" + route.request().fromCell().serial(), "uri" + route.request().toCell().serial());
+        this.usersWaitingForRoute.getOrDefault(p, new LinkedList<>()).forEach(u -> u.snd().writeTextMessage(routeAsJson));
         this.usersWaitingForRoute.remove(p);
     }
 }
