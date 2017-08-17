3139087dfa121d8cd1c38fab78b78b8c665f4003
albertogiunta <alb.giunta@gmail.com>
2017-08-17 17:03:49 +0200

    fix to controller

--- src/main/scala/admin/controller/CellTemplateController.scala
+++ src/main/scala/admin/controller/CellTemplateController.scala
@@ -63,7 +63,9 @@ class CellTemplateController extends ViewController {
     def setDynamicInformation(update: RoomDataUpdate): Unit = {
         currentPeopleValue setText update.currentPeople.toString
         update.cell.sensors.foreach(sensor => {
-            sensorsController.get(sensor.categoryId).get updateSensor sensor
+            if (sensorsController.contains(sensor.categoryId)) {
+                sensorsController.get(sensor.categoryId).get updateSensor sensor
+            }
         })
     }
 
